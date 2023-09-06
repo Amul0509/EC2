2e5042c8b675d0e7fd8d66f473a56b15d5628cac
Amul k <meet.rahul05@gmail.com>
2023-09-06 23:24:22 +0530

    jenkins file added

---
+++ Jenkins.tf
@@ -0,0 +1,36 @@
+#jenkins pipleine script
+pipeline {
+    
+    environment {
+        SECRET_VALUE = credentials('awskeys')
+    }
+    
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', credentialsId: 'github_cred', url: 'https://github.com/Amul0509/EC2.git'
+            }
+        }
+        stage('Terraform init') {
+            steps {
+                
+                sh 'terraform init'
+            
+            }
+        }
+        stage('Terraform plan') {
+            steps {
+                sh 'terraform plan'
+            }
+        }
+        stage('Terraform apply') {
+            steps {
+                
+                sh 'terraform apply --auto-approve'
+            }
+        }
+        
+    }
+}    
